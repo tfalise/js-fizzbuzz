edab1c5866e697561c04fe4df52248f9ba1d3012
Thibault Falise <thibault.falise@gmail.com>
2022-05-31 19:21:03 +0200

    Divisible by 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,8 @@
 function fizzbuzz(value) {
+    if(value % 3 == 0)
+    {
+        return "fizz"
+    }
     return value.toString();
 }
 
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -3,3 +3,7 @@ const fizzbuzz = require('./fizzbuzz');
 test('should return value as string when value is regular', () => {
     expect(fizzbuzz(1)).toBe("1");
 });
+
+test('should return fizz when value is divisible by 3', () => {
+    expect(fizzbuzz(3)).toBe("fizz");
+})
