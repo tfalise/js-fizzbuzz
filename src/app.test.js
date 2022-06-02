eea3f62cf2c0b01895f3846622909ab6aecde01b
Thibault Falise <thibault.falise@gmail.com>
2022-06-02 13:45:40 +0200

    Validate correct input

--- src/app.test.js
+++ src/app.test.js
@@ -11,3 +11,9 @@ test('negative number should not be valid input', () => {
 test('zero should not be valid input', () => {
     expect(parseInput(0).isValid).toBe(false);
 });
+
+test('positive integer should be valid input', () => {
+    let result = parseInput(4);
+    expect(result.isValid).toBe(true);
+    expect(result.value).toBe(4);
+});
